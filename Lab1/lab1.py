9b89a650e548f6cc470ed4b517638ccc08b74805
Speleon <lednicov2015@gmail.com>
2023-10-06 18:26:48 +0500

    Lab1+

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -2,31 +2,53 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
+def is_lying(k, b):
+  #проверка принадлежности прямой горизонтальной границе прямоугольника
+  if(math.isclose(k, 0, rel_tol = 1e-9)):
+      if(math.isclose(b, 3, rel_tol = 1e-9) or math.isclose(b, 4, rel_tol = 1e-9)):
+        return True
+      else:
+        return False
+      
 def is_intersecting(k, b):
+  #проверка пересечения прямой с горизонтальными границами прямоугольника
+  y1 = k * 1 + b
+  y2 = k * 2 + b
+
+  if (math.isclose(y1, 3, rel_tol = 1e-9) and math.isclose(y2, 4, rel_tol = 1e-9)) or \
+      (math.isclose(y2, 3, rel_tol = 1e-9) and math.isclose(y1, 4, rel_tol = 1e-9)):
+      return True
+  elif (math.isclose(k, 0, rel_tol = 1e-9) and 3 <= b <= 4):
+    return True
+  else:
+      return False
+  
+def count_intersection_points(k, b):
+  count = 0 #количество точек пересечения
+
+  #Если k == 0, то линия паралельна горизонтальным граням
+  if(k == 0):
+    if(b < 4 and b > 3):
+      count = 2
+    else:
+      count = 0
+  else:
+    #проверка пересечения прямой с вертикальными границами прямоугольника
+    x1 = (3 - b) / k
+    x2 = (4 - b) / k
+    if x1 is not None and 1 < x1 < 2:
+        count += 1
+    if x2 is not None and 1 < x2 < 2:
+        count += 1
     #проверка пересечения прямой с горизонтальными границами прямоугольника
     y1 = k * 1 + b
     y2 = k * 2 + b
-
-    if (math.isclose(y1, 3, rel_tol = 1e-9) and math.isclose(y2, 4, rel_tol = 1e-9)) or \
-       (math.isclose(y2, 3, rel_tol = 1e-9) and math.isclose(y1, 4, rel_tol = 1e-9)):
-        return True
-    else:
-        return False
-
-def count_intersection_points(k, b):
-    count = 0 #количество точек пересечения
-
-    #проверка пересечения прямой с вертикальными границами прямоугольника
-    x1 = (3 - b) / k if k != 0 else None
-    x2 = (4 - b) / k if k != 0 else None
-
-    if x1 is not None and 1 <= x1 <= 2:
+    if y1 is not None and 3 <= y1 <= 4:
+        count += 1
+    if y2 is not None and 3 <= y2 <= 4:
         count += 1
 
-    if x2 is not None and 1 <= x2 <= 2:
-        count += 1
-
-    return count
+  return count
 
 #ввод коэффициентов k и b
 k = float(input("Введите значение k: "))
@@ -49,13 +71,17 @@ plt.xlabel('x')
 plt.ylabel('y')
 
 #проверка на пересечение и колличества точек пересечения
-if is_intersecting(k, b):
-    intersection_count = count_intersection_points(k, b)
-    plt.title(f"Количество точек пересечения: {intersection_count}")
-    print("\nКоличество точек пересечения:", intersection_count)
+if is_lying(k, b):
+  plt.title(f"Горизонтальная граница прямоугольника лежит в прямой")
+  print("\nГоризонтальная граница прямоугольника лежит в прямой")
 else:
-    plt.title("Прямая не пересекается с прямоугольником")
-    print("\nПрямая не пересекается с прямоугольником")
+  intersection_count = count_intersection_points(k, b)
+  if intersection_count > 0:
+      plt.title(f"Количество точек пересечения: {intersection_count}")
+      print("\nКоличество точек пересечения:", intersection_count)
+  else:
+      plt.title("Прямая не пересекается с прямоугольником")
+      print("\nПрямая не пересекается с прямоугольником")
 
 plt.legend()
 plt.grid(True)
